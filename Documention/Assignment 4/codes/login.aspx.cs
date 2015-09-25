437a3c3bf274124c49f407a6445d6decf724f5ac
FaisalHakami <Faisalhakami55@gmail.com>
2015-09-24 21:55:00 -0600

    as 4
    Assignment 4

---
+++ Documention/Assignment 4/codes/login.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+     protected void Login_click(object sender, EventArgs e)
+        {
+            string name = Convert.ToString(Request["login"]);
+
+            Session["name"] = name;
+            Response.Redirect("Default.aspx");
+        }
+    }
+
